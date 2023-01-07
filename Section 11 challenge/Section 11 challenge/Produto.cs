ddd2c830b01d1ece7bc69c549051422b051f20b2
Patrick <patrickaquinodesouza@gmail.com>
2023-01-07 00:24:56 -0300

    Atualização

---
+++ Section 11 challenge/Section 11 challenge/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Globalization;
+namespace Section11
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Price { get; set; }
+        public int Qtd { get; set; }
+
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double price, int qtd)
+        {
+            Nome = nome;
+            Price = price;
+            Qtd = qtd;
+        }
+        public double Total()
+        {
+            return Price * Qtd;
+        }
+
+    }
+}
